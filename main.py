6ad796115e5890dcbda33f4163c9596313acd17d
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-11 17:42:46 +0600

    asp

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from error_log import log
-from scrapping import scrapping as s
+from scrapping import s
+
 
 log.set_custom_log_info("error_log/error.log")
 
 try:
     print("sp")
     var = 1 / 0
+    s = Student("Prothes")
+    s.display()
 except Exception as e:
     log.report(e)
 
--- scrapping/scrapping.py
+++
@@ -1,5 +0,0 @@
-class Student:
-    def __init__(self):
-        pass
-
-    x = 10
