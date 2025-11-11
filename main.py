ef57c7ebcadecbb8eaebac56afe8fe158ca3985d
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-11 17:49:21 +0600

    asp v6

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from error_log import log
-from scrapping import s
+from scrapping import scrapping as s
 
-
-log.set_custom_log_info("error_log/error.log")
-
-try:
-    print("sp")
-    var = 1 / 0
-    s = Student("Prothes")
-    s.display()
-except Exception as e:
-    log.report(e)
+s = s.Student("Prothes")
+s.display()
 
 
---
+++ scrapping/scrapping.py
@@ -0,0 +1,8 @@
+class Student:
+    x = 10
+
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print(f"Student Name : {self.name}")
